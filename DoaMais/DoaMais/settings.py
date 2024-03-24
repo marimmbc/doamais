f6ed6615a657d4d63d0b4524bbe78c74a911516d
Mariana Correia <mariana.correia.bc@gmail.com>
2024-03-24 15:09:17 -0300

    Adiconar Arquivos

---
+++ DoaMais/DoaMais/settings.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+BASE_DIR = settings.BASE_DIR
+SECRET_KEY = 'REPLACE_THIS_WITH_YOUR_SECRET_KEY'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'main',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
