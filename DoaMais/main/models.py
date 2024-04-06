eacc5869347c1599ec9573fdbe582c659241c11c
Mariana Correia <mariana.correia.bc@gmail.com>
2024-04-06 17:35:01 -0300

    change in view and registration and login models

--- DoaMais/main/models.py
+++ DoaMais/main/models.py
@@ -77,23 +77,6 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.username
 
-class RegisterForm(UserCreationForm):
-    
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-    def save(self, commit=True):
-        user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-
-        if commit:
-            user.save()
-        return user
-
-
 
 
 class Doacao(models.Model):
