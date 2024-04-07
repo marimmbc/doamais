26d4744a0f4fdf991f77c1529e0e2e011fb2bc17
Mariana Correia <mariana.correia.bc@gmail.com>
2024-04-07 13:26:14 -0300

    login_view change

--- DoaMais/main/views.py
+++ DoaMais/main/views.py
@@ -59,7 +59,7 @@ def login_view(request):
     else:
         form = AuthenticationForm()
 
-    return render(request, 'accounts/login.html', {'form': form})
+    return render(request, 'login.html', {'form': form})
 
 @login_required
 def perfil(request):
