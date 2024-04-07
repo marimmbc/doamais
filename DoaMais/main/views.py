542910d6c6206825f7ba37ee0a9bf5bd7eab2809
Mariana Correia <mariana.correia.bc@gmail.com>
2024-04-07 19:20:17 -0300

    alteração para funcionamento do login

--- DoaMais/main/views.py
+++ DoaMais/main/views.py
@@ -32,16 +32,16 @@ def cadastrar(request):
     return render(request, 'cadastrar.html')
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('pesquisar')
-        else:
-            messages.error(request, 'Invalid username or password!')
-    return render(request, 'login.html') 
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('pesquisar')
+    else:
+        messages.error(request, 'Invalid username or password!')
+    return render(request, 'login.html')
+
 
 @login_required
 def perfil(request):
